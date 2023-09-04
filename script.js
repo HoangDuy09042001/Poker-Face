37cd703bbbc190831e855c89924b996d78fd2a66
HoangDuy09042001 <duyht194748@sis.hust.edu.vn>
2023-09-04 11:26:17 +0700

    update

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.mtcnn.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-  faceapi.nets.tinyYolov2.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
@@ -24,7 +22,6 @@ video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    // const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options()).withFaceLandmarks().withFaceExpressions()
     detections.sort(function(a, b) {
       return b.detection.box.area - a.detection.box.area;
@@ -35,7 +32,7 @@ video.addEventListener('play', () => {
       const resizedDetections = faceapi.resizeResults(largestFace, displaySize)
       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
       faceapi.draw.drawDetections(canvas, resizedDetections)
-      // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
       faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     }else{
 
@@ -49,3 +46,57 @@ video.addEventListener('play', () => {
 
   }, 100)
 })
+
+// const video = document.getElementById('video')
+
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+//   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+//   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+//   faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+//   faceapi.nets.ageGenderNet.loadFromUri('/models')
+//  ]).then(startVideo)
+
+// startVideo()
+
+// function startVideo() {
+//   /*
+//   navigator.getUserMedia(
+//     { video: {} },
+//     stream => video.srcObject = stream,
+//     err => console.error(err)
+//   )
+//   */
+
+//   navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: false
+//   })
+//   .then(
+//     (cameraStream) => {
+//       video.srcObject = cameraStream;
+//     }
+//   )
+
+// }
+
+// video.addEventListener('play', () => {
+//   const canvas = faceapi.createCanvasFromMedia(video)
+//   document.body.append(canvas)
+//   const displaySize = { width: video.width, height: video.height }
+//   faceapi.matchDimensions(canvas, displaySize)
+//   setInterval(async () => {
+//     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
+//     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+//     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+//     faceapi.draw.drawDetections(canvas, resizedDetections)
+//     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+//     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+//     resizedDetections.forEach( detection => {
+//       const box = detection.detection.box
+//       const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+//       drawBox.draw(canvas)
+//     })
+
+//   }, 100)
+// })
